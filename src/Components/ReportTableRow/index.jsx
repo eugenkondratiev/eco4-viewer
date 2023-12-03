dc7678f829c1573bbb83e92b78ceb4c236977161
eugenkondratiev <45940679+eugenkondratiev@users.noreply.github.com>
2023-12-03 14:11:58 +0200

    add missed hours info in month report

--- src/Components/ReportTableRow/index.jsx
+++ src/Components/ReportTableRow/index.jsx
@@ -7,9 +7,14 @@ const ReportTableRow = ({ data, parameters }) => {
     return (
         <tr className={stl.root}>
             {data.map((value, columnIndex) => {
-                if (columnIndex === 0) return <td key={columnIndex}>{value}</td>
+                if (columnIndex === 0) {
+                    const [_hours, _hoursQuantity] = value.split("&")
+                    console.log(value, value.split("&"));
+                    if (_hoursQuantity) console.log(_hours, _hoursQuantity);
+                    return <td key={columnIndex}>{_hours}{_hoursQuantity && <p className={stl.superscript}>{_hoursQuantity}</p>}</td>
+                }
                 const digitAfterDot = parameters[columnIndex - 1].format.split(".")[1]
-                if (!parameters || !parameters[columnIndex-1]) console.log("Problem value -", columnIndex, parameters[columnIndex - 1]);
+                if (!parameters || !parameters[columnIndex - 1]) console.log("Problem value -", columnIndex, parameters[columnIndex - 1]);
                 return <td key={columnIndex}>{parameters
                     ? Number(value).toFixed(
                         digitAfterDot
